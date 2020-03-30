e1c03fa9f7188adc57f424c60bc921277bc47e1b
Lycra-98 <1260352026@qq.com>
2020-03-30 16:42:00 +0800

    完成项目

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-import Cate from './model/CateEnum'
-import ItemData from './model/ItemData'
-// import DataHelper from './store/DataHelper'
-import ActionHelper from './store/ActionHelper'
 
 Vue.config.productionTip = false
 
@@ -12,11 +8,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
-
-// let item1 = new ItemData(-1, Cate.Life, '许愿春招上岸', '我是真的想要上岸鸭')
-// let ietm2 = new ItemData(1, Cate.Study, '好好学习前端', '我要学好前端然后冲冲冲')
-// let ah = new ActionHelper();
-// ah.add(item1);
-// ah.edit(ietm2)
-// ah.remove(4)
-// console.log(ah.memoList);
